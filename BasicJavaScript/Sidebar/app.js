882af49421f59fa942680737bc2ccedb8574a0e8
ssoherb <ssoworldy@gmail.com>
2023-11-09 13:44:26 +0900

    Sidebar

---
+++ BasicJavaScript/Sidebar/app.js
@@ -0,0 +1,17 @@
+const toggle = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+toggle.addEventListener("click", () => {
+  // console.log(sidebar.classList);
+  /* if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  } */
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
