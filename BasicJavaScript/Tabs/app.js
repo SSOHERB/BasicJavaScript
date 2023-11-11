d80be44ccad9ff594f776ca7787bdb5f582c8f9b
ssoherb <ssoworldy@gmail.com>
2023-11-11 17:07:56 +0900

    Tabs

---
+++ BasicJavaScript/Tabs/app.js
@@ -0,0 +1,27 @@
+const tabswrap = document.querySelector(".tabs-wrap");
+const btns = document.querySelectorAll(".tab-btn");
+const tabs = document.querySelectorAll(".tab");
+
+tabswrap.addEventListener("click", (e) => {
+  // 클릭시 이벤가 발생한 요소의 data-id 값 가져오기
+  const id = e.target.dataset.id;
+  // console.log(id);
+
+  if (id) {
+    // 선택된 다른 버튼들 제거
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    // 다른 ta에 active 우선 제거
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    // .tab의 속성중 id를 가져옴
+    const element = document.getElementById(id);
+    // console.log(element);
+    element.classList.add("active");
+  }
+});
